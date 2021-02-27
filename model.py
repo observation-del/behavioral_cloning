9d36250bc0711e15183a441d3f61fe75474c921c
Makoto Sekiguchi <observation84@gmail.com>
2021-02-27 18:57:38 +0900

    add generator function

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 import numpy as np
+import os
 import csv
 import cv2
+import sklearn
+from math import ceil
 from scipy import ndimage
 
 lines  = []
@@ -10,45 +13,106 @@ with open('./data/driving_log.csv') as csvfile:
     
     for line in reader:
         lines.append(line)
-        
-images = []
-measurements = []
-for line in lines:
-    # create adjusted steering measurements for the side camera images
+
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+train_samples, validation_samples = train_test_split(lines)
+
+def generator(samples, batch_size):
+    num_sumples = len(samples)
     correction = 0.2
-    measurement_center = float(line[3])
-    measurement_left = measurement_center + 0.2
-    measurement_right = measurement_center - 0.2
-    measurement_tmp = [measurement_center, measurement_left, measurement_right]
+    while 1: # Loop forever so the generator never terminates
+        samples = shuffle(samples)
+        for offset in range(0, num_sumples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            measurements = []
+            for batch_sample in batch_samples:
+                measurement_center = float(line[3])
+                measurement_left = measurement_center + 0.2
+                measurement_right = measurement_center - 0.2
+                measurement_tmp = [measurement_center, measurement_left, measurement_right]
+                
+                source_path_center = line[0]
+                source_path_left = line[1]
+                source_path_right = line[2]
+                filename_center = source_path_center.split('/')[-1]
+                filename_left = source_path_left.split('/')[-1]
+                filename_right = source_path_right.split('/')[-1]
+                path_center = './data/IMG/' + filename_center
+                path_left = './data/IMG/' + filename_left
+                path_right = './data/IMG/' + filename_right
+
+                # image = cv2.imread(current_path)
+                image_center = ndimage.imread(path_center)
+                image_left = ndimage.imread(path_left)
+                image_right = ndimage.imread(path_right)
+                image_tmp = [image_center, image_left, image_right]
+
+                images.extend(image_tmp)
+                measurements.extend(measurement_tmp)
+
+            augmented_images, augmented_measurements = [], []
+            for image, measurement in zip(images, measurements):
+                augmented_images.append(image)
+                augmented_measurements.append(measurement)
+                augmented_images.append(cv2.flip(image,1))
+                augmented_measurements.append(measurement*-1.0)
+
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_measurements)
+
+            yield shuffle(X_train, y_train)
+
+
+# images = []
+# measurements = []
+# for line in lines:
+#     # create adjusted steering measurements for the side camera images
+#     correction = 0.2
+#     measurement_center = float(line[3])
+#     measurement_left = measurement_center + 0.2
+#     measurement_right = measurement_center - 0.2
+#     measurement_tmp = [measurement_center, measurement_left, measurement_right]
     
-    source_path_center = line[0]
-    source_path_left = line[1]
-    source_path_right = line[2]
-    filename_center = source_path_center.split('/')[-1]
-    filename_left = source_path_left.split('/')[-1]
-    filename_right = source_path_right.split('/')[-1]
-    path_center = './data/IMG/' + filename_center
-    path_left = './data/IMG/' + filename_left
-    path_right = './data/IMG/' + filename_right
+#     source_path_center = line[0]
+#     source_path_left = line[1]
+#     source_path_right = line[2]
+#     filename_center = source_path_center.split('/')[-1]
+#     filename_left = source_path_left.split('/')[-1]
+#     filename_right = source_path_right.split('/')[-1]
+#     path_center = './data/IMG/' + filename_center
+#     path_left = './data/IMG/' + filename_left
+#     path_right = './data/IMG/' + filename_right
 
-    # image = cv2.imread(current_path)
-    image_center = ndimage.imread(path_center)
-    image_left = ndimage.imread(path_left)
-    image_right = ndimage.imread(path_right)
-    image_tmp = [image_center, image_left, image_right]
+#     # image = cv2.imread(current_path)
+#     image_center = ndimage.imread(path_center)
+#     image_left = ndimage.imread(path_left)
+#     image_right = ndimage.imread(path_right)
+#     image_tmp = [image_center, image_left, image_right]
 
-    images.extend(image_tmp)
-    measurements.extend(measurement_tmp)
+#     images.extend(image_tmp)
+#     measurements.extend(measurement_tmp)
 
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image,1))
-    augmented_measurements.append(measurement*-1.0)
+# augmented_images, augmented_measurements = [], []
+# for image, measurement in zip(images, measurements):
+#     augmented_images.append(image)
+#     augmented_measurements.append(measurement)
+#     augmented_images.append(cv2.flip(image,1))
+#     augmented_measurements.append(measurement*-1.0)
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+# X_train = np.array(augmented_images)
+# y_train = np.array(augmented_measurements)
+
+# Set the batch size
+batch_size = 32
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)
+
+ch, row, col = 3, 160, 320
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
@@ -56,7 +120,7 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 127.5 - 1., input_shape=(row, col, ch), output_shape=(row, col, ch)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
@@ -70,6 +134,10 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+model.fit_generator(train_generator,
+            steps_per_epoch=ceil(len(train_samples)/batch_size),
+            validation_data=validation_generator,
+            validation_steps=ceil(len(validation_samples)/batch_size),
+            epochs=5, verbose=1)
 
 model.save('model.h5')
